06126b6088d04f78b639d0fae6e06c0d7ea1a4cb
Songlin Hou <shou@wpi.edu>
2022-06-08 01:00:56 -0400

    add tf 2

---
+++ environments/tensorflow2_gpu_jupyter/build_image.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+USER_NAME=$(whoami)
+source ../docker_permission_fix.sh
+CONTAINER_NAME="tensorflow_1_gpu:$USER_NAME"
+mkdir -p container_names
+echo "Jupyter Notebook with Tensorflow 1 Image Builder"
+
+echo "Input the name of container"
+read CONTAINER_NAME_PREFIX
+CONTAINER_NAME="$CONTAINER_NAME_PREFIX:$USER_NAME"
+
+echo "Using $CONTAINER_NAME as container name"
+sleep 3
+docker build -f dockerfile -t $CONTAINER_NAME .
+
+if [ $? -eq 0 ]
+then
+    echo "Image $CONTAINER_NAME is built successfully"
+    echo $CONTAINER_NAME_PREFIX > "container_names/$USER_NAME"
+else
+    echo "Error in building image $CONTAINER_NAME"
+fi
